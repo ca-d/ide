eff615e373c96327fab5c50fb89da7b94c34165c
cad <cad@sdf.org>
2021-07-02 23:08:00 +0200

    Update editor.ts

--- editor.ts
+++ editor.ts
@@ -1,34 +1,34 @@
 const handlers = { GET: handleGet };
 const routers = {
-	GET: [
-		['/store', handleStore],
-		['/', handleGet]
-	] };
-	
+  GET: [
+    ['/store', handleStore],
+    ['/', handleGet]
+  ] };
+  
 async function handleStore(request) {
-	return new Response('<p>hai store</p>',
-		{ headers: { "content-type": "text/html; charset=UTF-8" } });
+  return new Response('<p>hai store</p>',
+    { headers: { "content-type": "text/html; charset=UTF-8" } });
 }
 
 async function handleGet(request) {
-	return new Response(html, {
-		headers: {
-			"content-type": "text/html; charset=UTF-8",
-		},
-	});
+  return new Response(html, {
+    headers: {
+      "content-type": "text/html; charset=UTF-8",
+    },
+  });
 }
 
 async function handleRequest(request) {
-	const routes = routers[request.method];
-	if (!routes)
-	return new Response(null, {
-		status: 405,
-		statusText: "Method Not Allowed",
-	});
-	
-	const handler = routes.find(([prefix, _]) =>
-		new URL(request.url).pathname.startsWith(prefix))[1];
-	return handler(request);
+  const routes = routers[request.method];
+  if (!routes)
+  return new Response(null, {
+    status: 405,
+    statusText: "Method Not Allowed",
+  });
+  
+  const handler = routes.find(([prefix, _]) =>
+    new URL(request.url).pathname.startsWith(prefix))[1];
+  return handler(request);
 }
 
 const html = `<html>
@@ -37,15 +37,15 @@ const html = `<html>
 </head>
 <body>
   <script type="module"
-  	src="https://unpkg.com/ace-custom-element@latest/dist/index.min.js">
+    src="https://unpkg.com/ace-custom-element@latest/dist/index.min.js">
   </script>
 
-	<ace-editor theme="ace/theme/solarized_dark"
-							mode="ace/mode/typescript"
-							value="console.log('hello world');">
-	</ace-editor>
-	
-	<script>
+  <ace-editor theme="ace/theme/solarized_dark"
+              mode="ace/mode/typescript"
+              value="console.log('hello world');">
+  </ace-editor>
+  
+  <script>
     function copyToClipboard(message) {
         var textArea = document.createElement("textarea");
         textArea.value = message;
@@ -77,20 +77,20 @@ const html = `<html>
           copyToClipboard(url);
         }
       }, true);
-	</script>
+  </script>
 
-	<style>
-	@import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;600&display=swap');
-	body {
-	  margin: 0px;
-	}
-	ace-editor.ace_editor {
-	    width: 100vw;
-	    height: 100vh;
-	    font-size: 20px;
-	    font-family: 'Fira Code';
-	}
-	</style>
+  <style>
+  @import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;600&display=swap');
+  body {
+    margin: 0px;
+  }
+  ace-editor.ace_editor {
+      width: 100vw;
+      height: 100vh;
+      font-size: 20px;
+      font-family: 'Fira Code';
+  }
+  </style>
 </body>
 </html>`;
 
